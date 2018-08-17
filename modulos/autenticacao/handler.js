6f5448cb8927e72df7d6a487a0f7535c585be5b9
Luis Felipe Assumpção Fleury <felipe@xmsistemas.com.br>
2018-08-17 15:52:02 -0300

    Primeira versão funcional

--- modulos/autenticacao/handler.js
+++ modulos/autenticacao/handler.js
@@ -1,18 +1,20 @@
 'use strict';
 const AWS = require('aws-sdk');
+const jwt = require("jsonwebtoken");
 
 const USUARIOS_TABLE = process.env.USUARIOS_TABLE;
 const AWS_DEPLOY_REGION = process.env.AWS_DEPLOY_REGION;
+const JWT_ENCRYPTION_CODE = process.env.JWT_ENCRYPTION_CODE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
     api_version: '2012-08-10',
     region: AWS_DEPLOY_REGION
 });
 
 // Create the DynamoDB service object
-ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
 
 module.exports.authenticate = async (event, context) => {
-
+  
   let _parsed;
   try {
     _parsed = JSON.parse(event.body);
@@ -25,20 +27,15 @@ module.exports.authenticate = async (event, context) => {
   }
   const { username, password } = _parsed;
 
+
+  var key = { "username": username };
   const params = {
-    Key: {
-      "username": {
-        S: username
-       }, 
-      "SongTitle": {
-        S: "Happy Day"
-       }
-     }, 
+    Key: key, 
     TableName: USUARIOS_TABLE
   };
 
   return await new Promise((resolve, reject) => {
-    ddb.getItem(params, (error, data) => {
+    dynamoDb.get(params, (error, data) => {
       if (error) {
         console.log(`autenticate ERROR=${error.stack}`);
           resolve({
@@ -47,8 +44,20 @@ module.exports.authenticate = async (event, context) => {
           });
   
       } else {
-        console.log(`autenticate data=${JSON.stringify(data)}`);
-        resolve({ statusCode: 200, body: JSON.stringify(data) });
+        var senha = data["Item"]["password"];
+        console.log(senha);
+        console.log(password);
+        console.log(`autenticate data=${senha}`);
+        if (password == senha) {
+          // caso a senha do usuário seja encontrada.... iremos criar um token:
+          var token = jwt.sign(data, JWT_ENCRYPTION_CODE, {
+            expiresIn: '1h' //o token irá expirar em 24 horas
+          });
+          console.log(token);
+          resolve({ statusCode: 200, body: JSON.stringify(token) });
+        } else {
+          resolve({ statusCode: 400, body: "Usuario ou senha inválida" });
+        }
       }
     });
   });
