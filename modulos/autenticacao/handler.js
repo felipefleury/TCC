4c21ad5287b5badf5825873bacd91b5a898a94fa
= <=>
2018-08-21 09:01:23 -0300

    Incluindo verificacao de usuario existente. Ainda falta varias correcoes

--- modulos/autenticacao/handler.js
+++ modulos/autenticacao/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 const AWS = require('aws-sdk');
 const jwt = require("jsonwebtoken");
+const uuid = require("uuid");
 
 const USUARIOS_TABLE = process.env.USUARIOS_TABLE;
 const AWS_DEPLOY_REGION = process.env.AWS_DEPLOY_REGION;
@@ -75,15 +76,62 @@ module.exports.createUser = async (event, context) => {
       error: `Could not parse requested JSON: ${err.stack}`
     };
   }
+
   const { username, password, nome, tipo, email } = _parsed;
 
+  var teste = {..._parsed, teste:1 }
+
+  console.log(teste);
+
+  if(!validateUser(username, password, nome, tipo, email)) {
+    return {
+      statusCode: 500,
+      error: 'Not valid!'
+    }
+  }
+  console.log("Procurando usuario!");
+  
+
+  var value = await getUserByLogin(username);
+  console.log(value);
+  console.log(value.Count);
+
+  if(value.Count > 0) {
+    console.log("usuario existente!");
+    return {
+      statusCode: 500,
+      error: "Existente!"
+    }
+  } 
+  var user = userInfo(username, password, nome, tipo, email);
+  
+  
+  
+  saveUsuario(user)
+  .then(result => {
+    console.log(result);
+    return{
+      statusCode: 204
+    }
+  }).catch(error => {
+    console.log(error);
+    return{
+      statusCode: 400
+    }
+  })
+  
+  
+};
+
+
+const saveUsuario = (userInfo) => {
   const params = {
     TableName: USUARIOS_TABLE,
-    Item: {
-      username, password, nome, tipo, email
-    },
+    Item: userInfo
   };
 
+  return dynamoDb.put(params).promise();
+  /* 
   return await new Promise((resolve, reject) => {
     dynamoDb.put(params, (error, data) => {
       if (error) {
@@ -99,4 +147,80 @@ module.exports.createUser = async (event, context) => {
       }
     });
   });
+  */
+}
+
+const validateUser = (username, password, nome, tipo, email) => {
+  if (typeof username !== 'string' || typeof password !== 'string' || typeof nome !== 'string' || typeof tipo !== 'string' || typeof email !== 'string') {
+    console.error('Validation Failed');
+    return false;
+  }
+  return true;
+}
+
+const getUserByLogin = (username) => {
+
+  var key = { "username": username };
+  /*
+  const params = {
+    Key: key, 
+    IndexName: "username-index",
+    TableName: USUARIOS_TABLE
+  };
+*/
+
+  const params = {
+  TableName: USUARIOS_TABLE,
+  IndexName: "username-index",
+    ProjectionExpression:'username', // remove this string if you want to get not only 'name'
+    FilterExpression:'username = :username',
+    ExpressionAttributeValues:{ ":username" : username }
+
+  }
+/*
+payload = {
+    TableName: USUARIOS_TABLE,
+    IndexName: "email",
+    KeyConditionExpression: "#index = :index_value",
+    ExpressionAttributeNames:{
+        "#index": "email"
+    },
+    ExpressionAttributeValues: {
+        ":index_value": "test@gmail.com" // <----------------
+    },
+    ProjectionExpression: "user_id",
+        ScanIndexForward: false
+    };
+}
+*/
+  console.log("buscar dados");
+  //return dynamoDb.get(params).promise();
+
+  return new Promise((resolve, reject) => {
+    dynamoDb.scan(params, (error, data) => {
+      console.log("teste");
+      if (error) {
+        console.log(`getUserByLogin ERROR=${error.stack}`);
+          reject(error);
+      } else {
+        console.log(data);
+          resolve(data);
+      }
+    });
+  });
+  
+} 
+
+const userInfo = (username, password, nome, tipo, email) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: uuid.v1(),
+    username: username,
+    email: email,
+    nome: nome,
+    password: password,
+    tipo: tipo,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+  };
 };
